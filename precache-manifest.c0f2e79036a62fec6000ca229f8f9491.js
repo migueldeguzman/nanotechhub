2511c863b610ae170e792214be9c8a8772a7defa
migueldeguzman <40692613+migueldeguzman@users.noreply.github.com>
2018-10-07 15:19:09 +0800

    new build

---
+++ precache-manifest.c0f2e79036a62fec6000ca229f8f9491.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2689ae14bc6ad9c2585bc13224c7f636",
+    "url": "/static/media/logo.2689ae14.svg"
+  },
+  {
+    "revision": "9d9679f3083de083188888d35e333d45",
+    "url": "/static/media/background.9d9679f3.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7ba775ed077ce98b58b8",
+    "url": "/static/js/main.7ba775ed.chunk.js"
+  },
+  {
+    "revision": "883867c7165a7c4031d7",
+    "url": "/static/js/1.883867c7.chunk.js"
+  },
+  {
+    "revision": "7ba775ed077ce98b58b8",
+    "url": "/static/css/main.b7b7b6ad.chunk.css"
+  },
+  {
+    "revision": "883867c7165a7c4031d7",
+    "url": "/static/css/1.54c32c8a.chunk.css"
+  },
+  {
+    "revision": "2fc005e25adc329860216c8e058a6442",
+    "url": "/index.html"
+  }
+];
